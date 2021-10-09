187c7fd81b4b0c76c33f4413a4b91f14cc748e4a
enggar appkey <enggar.appkey@gmail.com>
2021-10-09 16:50:22 +0800

    :construction: WIP: add login filter

--- app/Http/Controllers/OauthController.php
+++ app/Http/Controllers/OauthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Oauth;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Laravel\Socialite\Facades\Socialite;
@@ -16,16 +17,51 @@ class OauthController extends Controller
 
     public function google()
     {
-        $user = Socialite::driver('google')->user();
+        $oauthData = Socialite::driver('google')->user();
 
-        Oauth::firstOrCreate([
-            'user_id'   => Auth::id(),
-            'provider'  => 'google',
-            'access_token'  => $user->token,
-            'refresh_token' => $user->refreshToken
-        ]);
+        if (Auth::user()) {
 
-        return redirect()->route('home');
+            Oauth::firstOrCreate([
+                'user_id'   => Auth::id(),
+                'provider'  => 'google',
+                'access_token'  => $oauthData->token,
+                'refresh_token' => $oauthData->refreshToken
+            ]);
+
+            return redirect()->route('home');
+        }
+
+        // Check if user already has account
+        $users = User::whereHas('oauth', function ($query) use (&$oauthData) {
+            $query->where('access_token', $oauthData->token);
+        })->first();
+
+        dd($oauthData);
+
+        if ($users) {
+            Auth::loginUsingId($users->id);
+
+            return redirect()->route('home');
+        }
+
+        // // Create User
+        // $createUser = User::create([
+        //     'name' => $oauthData->getName(),
+        //     'email' => $oauthData->getEmail(),
+        // ]);
+
+        // $createUser->oauth()->create([
+        //     'provider'  => 'google',
+        //     'access_token'  => $oauthData->token,
+        //     'refresh_token' => $oauthData->refreshToken
+        // ]);
+
+        // Auth::loginUsingId($createUsers->id);
+
+        // return redirect()->route('home');
+
+        return redirect()->route('login');
+
     }
 
     public function facebook()
